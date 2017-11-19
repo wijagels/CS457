0505b13b3284b1ebab9df424451b1b8130a5c081
William Jagels <william@jagels.us>
2017-11-19 04:00:46 -0500

    Restructure test

--- kv/server/test/log_test.cpp
+++ kv/server/test/log_test.cpp
@@ -15,13 +15,24 @@ TEST(Log, single_write) {  // NOLINT
     EXPECT_EQ(sm.do_write().uuid(), "Hello world") << "Deserialized data should match input";
     runs++;
   });
+  svc.run();
+  EXPECT_EQ(runs, 2) << "Both callbacks should happen";
+}
+
+TEST(Log, multi_write) {  // NOLINT
+  boost::asio::io_service svc;
+  auto log = std::make_shared<Log>("testing.log", svc);
+  log->clear();
+  auto runs = 0;
   kvstore::server::ServerMessage msg2;
   msg2.mutable_do_write()->set_uuid("Hello mundo");
   log->write_update(msg2, []() {});
   log->write_update(msg2, []() {});
   log->write_update(msg2, []() {});
-  log->replay_log(
-      [](kvstore::server::ServerMessage &&sm) { std::cout << sm.do_write().uuid() << std::endl; });
+  log->replay_log([&runs](kvstore::server::ServerMessage &&sm) {
+    EXPECT_EQ(sm.do_write().uuid(), "Hello mundo");
+    runs++;
+  });
   svc.run();
-  EXPECT_EQ(runs, 2) << "Both callbacks should happen";
+  EXPECT_EQ(runs, 3) << "Replay should get 3 calls";
 }
